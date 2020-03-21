45cc08f67b0deea8745350595cc0b068bae15ef3
Baiyuan Wang <bywang@thoughtworks.com>
2020-03-21 11:24:42 +0800

    Add eslint

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import ExampleComponent from 'react-scheduler-calendar'
+import ExampleComponent from 'react-scheduler-calendar';
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent text="Modern React component module" />
       </div>
-    )
+    );
   }
 }
