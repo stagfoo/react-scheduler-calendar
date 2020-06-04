f1a46c0ae9944b9ab281a0a34a2c3132d3f4adff
Baiyuan Wang <bywang@thoughtworks.com>
2020-06-04 09:57:29 +0800

    drag and drop support touch

--- src/lib/withDnDContext.tsx
+++ src/lib/withDnDContext.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 
 export const withDnDContext = <P extends {}>(PureComponent: React.ComponentType<P>): React.FC<P> => {
+  const supportTouch = 'ontouchstart' in window;
+  const backend = supportTouch ? TouchBackend : HTML5Backend;
   return (props) => (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={backend}>
       <PureComponent {...props}/>
     </DndProvider>
   );
