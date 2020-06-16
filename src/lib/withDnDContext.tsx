76b42f3d427cd2dfade67c35860a833eb654cdd2
qyzhao <qyzhao@thoughtworks.com>
2020-06-16 09:26:28 +0800

    remove touch backend for no need that for now

--- src/lib/withDnDContext.tsx
+++ src/lib/withDnDContext.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import { TouchBackend } from 'react-dnd-touch-backend';
 
 export const withDnDContext = <P extends {}>(PureComponent: React.ComponentType<P>): React.FC<P> => {
-  const supportTouch = 'ontouchstart' in window;
-  const backend = supportTouch ? TouchBackend : HTML5Backend;
   return (props) => (
-    <DndProvider backend={backend}>
+    <DndProvider backend={HTML5Backend}>
       <PureComponent {...props}/>
     </DndProvider>
   );
