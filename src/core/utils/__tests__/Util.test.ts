4da69f1f1a63f3f5648adbb8c2861f11e6b2e556
qyzhao <qyzhao@thoughtworks.com>
2020-06-23 10:51:48 +0800

    add test case for get position

--- src/core/utils/__tests__/Util.test.ts
+++ src/core/utils/__tests__/Util.test.ts
@@ -1,7 +1,13 @@
-import { getScrollSpeedRate } from '../Util';
+import { getPos, getScrollSpeedRate } from '../Util';
 
 describe('get scroll speed rate', () => {
   it('should return 3 given distance and maxDistance 6 add 10', () => {
     expect(getScrollSpeedRate(5, 10)).toEqual(Math.exp(1));
+  });
+});
+
+describe('get position', () => {
+  it('should return 0 0 when given element null', () => {
+    expect(getPos(null)).toEqual({ x: 0, y: 0 });
   });
 });
