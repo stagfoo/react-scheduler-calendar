4853f2755912e2661456bd5772246e77e099c16b
qyzhao <qyzhao@thoughtworks.com>
2020-06-23 09:53:26 +0800

    update script and support import module absolute path

--- src/core/components/__tests__/selectedArea.test.tsx
+++ src/core/components/__tests__/selectedArea.test.tsx
@@ -1,7 +1,7 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import SelectedArea from '../SelectedArea';
-import '../../../setupTest';
+import 'src/setupTest';
 
 describe('selected area', () => {
   it('should render correctly', () => {
