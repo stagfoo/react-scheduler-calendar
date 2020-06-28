47a97d15462589197363e25cf56b865b889e1c29
muzrea <muzrea@hotmail.com>
2020-06-28 10:31:26 +0800

    test: add test case for change popOver visible

--- src/core/components/__tests__/Header.test.tsx
+++ src/core/components/__tests__/Header.test.tsx
@@ -1,9 +1,23 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import moment from 'moment';
 import React from 'react';
-import 'src/setupTest';
 import { views } from 'src/core/components/_mockData/config';
+import 'src/setupTest';
 import Header, { Props } from '../Header';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
 
 describe('selected area', () => {
   let props: Props;
@@ -37,5 +51,14 @@ describe('selected area', () => {
   it('should render correctly', () => {
     const wrapper = shallow(<Header {...props}/>);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should change visible when call onVisibleChange', () => {
+    const wrapper = mount(<Header {...props}/>);
+    const onVisibleChange = wrapper.find('Popover').prop('onVisibleChange') as () => void;
+
+    onVisibleChange();
+
+    expect(wrapper.find('Popover').prop('visible')).toEqual(false);
   });
 });
